47fd5b761fa1d524fd52d490f116d57e7d7a8c58
Varun <varundarling7165@gmail.com>
2025-01-23 18:11:34 +0530

    Added responsive layout for every screen size

--- app/src/main/java/com/varun/shoppingapp/Controller/ProductsActivity.kt
+++ app/src/main/java/com/varun/shoppingapp/Controller/ProductsActivity.kt
@@ -1,20 +1,44 @@
 package com.varun.shoppingapp.Controller
 
+import android.content.res.Configuration
 import android.nfc.cardemulation.CardEmulation.EXTRA_CATEGORY
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.varun.shoppingapp.Adapter.ProductsAdapter
 import com.varun.shoppingapp.R
+import com.varun.shoppingapp.Services.DataService
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter : ProductsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_products)
 
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
+        adapter = ProductsAdapter(this,DataService.getProducts(categoryType.toString()))
+
+        var spanCount = 2
+        val orientation = resources.configuration.orientation
+        if(orientation == Configuration.ORIENTATION_LANDSCAPE){
+            spanCount = 3
+        }
+
+        val screenSize = resources.configuration.screenWidthDp
+        if(screenSize > 720){
+            spanCount = 3
+        }
+
+        val layoutManager = GridLayoutManager(this,spanCount)
+        findViewById<RecyclerView>(R.id.ProductListView).layoutManager = layoutManager
+        findViewById<RecyclerView>(R.id.ProductListView).adapter = adapter
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
--- app/src/main/java/com/varun/shoppingapp/Services/DataService.kt
+++ app/src/main/java/com/varun/shoppingapp/Services/DataService.kt
@@ -7,31 +7,77 @@ import com.varun.shoppingapp.Model.Product
 object DataService {
 
     val categories = listOf(
-        Category("SHIRTS","shirtimage"),
-        Category("HOODIES","hoodieimage"),
-        Category("HATS","hatimage"),
-        Category("DIGITAL","digitalgoodsimage"),
+        Category("SHIRTS", "shirtimage"),
+        Category("HOODIES", "hoodieimage"),
+        Category("HATS", "hatimage"),
+        Category("DIGITAL", "digitalgoodsimage"),
+        Category("SHIRTS", "shirtimage"),
+        Category("HOODIES", "hoodieimage"),
+        Category("HATS", "hatimage"),
+        Category("DIGITAL", "digitalgoodsimage"),
+        Category("SHIRTS", "shirtimage"),
+        Category("HOODIES", "hoodieimage"),
+        Category("HATS", "hatimage"),
+        Category("DIGITAL", "digitalgoodsimage")
     )
 
     val hats = listOf(
-        Product("Triangle Hat with logo","₹299","hat1"),
-        Product("Black Cap with logo","₹199","hat2"),
-        Product("White Cap with logo","₹399","hat3"),
-        Product("Latest Cap with logo","₹499","hat4"),
+        Product("Triangle Hat with logo", "₹299", "hat1"),
+        Product("Black Cap with logo", "₹199", "hat2"),
+        Product("White Cap with logo", "₹399", "hat3"),
+        Product("Latest Cap with logo", "₹499", "hat4"),
+        Product("Triangle Hat with logo", "₹299", "hat1"),
+        Product("Black Cap with logo", "₹199", "hat2"),
+        Product("White Cap with logo", "₹399", "hat3"),
+        Product("Latest Cap with logo", "₹499", "hat4"),
+        Product("Triangle Hat with logo", "₹299", "hat1"),
+        Product("Black Cap with logo", "₹199", "hat2"),
+        Product("White Cap with logo", "₹399", "hat3"),
+        Product("Latest Cap with logo", "₹499", "hat4")
     )
 
     val hoodies = listOf(
-        Product("Black Hoodie with logo","₹999","hoodie1"),
-        Product("Red Hoodie with logo","₹1099","hoodie2"),
-        Product("Black custom with logo","₹899","hoodie3"),
-        Product("Black with red logo","₹1199","hoodie4")
+        Product("Black Hoodie with logo", "₹999", "hoodie1"),
+        Product("Red Hoodie with logo", "₹1099", "hoodie2"),
+        Product("Black custom with logo", "₹899", "hoodie3"),
+        Product("Black with red logo", "₹1199", "hoodie4"),
+        Product("Black Hoodie with logo", "₹999", "hoodie1"),
+        Product("Red Hoodie with logo", "₹1099", "hoodie2"),
+        Product("Black custom with logo", "₹899", "hoodie3"),
+        Product("Black with red logo", "₹1199", "hoodie4"),
+        Product("Black Hoodie with logo", "₹999", "hoodie1"),
+        Product("Red Hoodie with logo", "₹1099", "hoodie2"),
+        Product("Black custom with logo", "₹899", "hoodie3"),
+        Product("Black with red logo", "₹1199", "hoodie4")
     )
 
     val shirts = listOf(
-        Product("Black T-Shirt with logo","₹299","shirt1"),
-        Product("Grey T-Shirt with logo","₹399","shirt2"),
-        Product("Red T-Shirt with logo","₹499","shirt3"),
-        Product("Light Black T-Shirt with logo","₹199","shirt4"),
-        Product("Black T-Shirt with new logo","₹499","shirt5")
+        Product("Black T-Shirt with logo", "₹299", "shirt1"),
+        Product("Grey T-Shirt with logo", "₹399", "shirt2"),
+        Product("Red T-Shirt with logo", "₹499", "shirt3"),
+        Product("Light Black T-Shirt with logo", "₹199", "shirt4"),
+        Product("Black T-Shirt with new logo", "₹499", "shirt5"),
+        Product("Black T-Shirt with logo", "₹299", "shirt1"),
+        Product("Grey T-Shirt with logo", "₹399", "shirt2"),
+        Product("Red T-Shirt with logo", "₹499", "shirt3"),
+        Product("Light Black T-Shirt with logo", "₹199", "shirt4"),
+        Product("Black T-Shirt with new logo", "₹499", "shirt5"),
+        Product("Black T-Shirt with logo", "₹299", "shirt1"),
+        Product("Grey T-Shirt with logo", "₹399", "shirt2"),
+        Product("Red T-Shirt with logo", "₹499", "shirt3"),
+        Product("Light Black T-Shirt with logo", "₹199", "shirt4"),
+        Product("Black T-Shirt with new logo", "₹499", "shirt5")
     )
+
+    val digitalGooods = listOf<Product>()
+
+    fun getProducts(category: String): List<Product> {
+
+        return when (category) {
+            "SHIRTS" -> shirts
+            "HOODIES" -> hoodies
+            "HATS" -> hats
+            else -> digitalGooods
+        }
+    }
 }
